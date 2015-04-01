e579cac573e482b2d4454db6ad2a908caeadf37c
iksoiks <christian.cricri94@gmail.com>
2015-04-01 23:06:23 +0200

    Event Schema Created

---
+++ models/event.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+var EventSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    place: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number
+    },
+    link: {
+        type: String
+    },
+    datetime: {
+        type: Date,
+        required: true
+    }
+});
